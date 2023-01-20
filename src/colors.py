3cd7d628b71bea13c001a4423939c807c398d91d
yasserprogamer <71287199+yasserprogamer@users.noreply.github.com>
2023-01-20 01:16:33 +0100

    small fix

--- src/colors.py
+++ src/colors.py
@@ -1,6 +1,6 @@
 import Config
 
 def fcolor(r:int, g:int, b:int) -> str:
-    if not bool(Config.Getconfig("Settings", "Use-Colors")):
+    if str(Config.Getconfig("Settings", "Use-Colors")).lower().replace(" ", "") != "true":
         return ""
     return f"\033[38;2;{r};{g};{b}m"
